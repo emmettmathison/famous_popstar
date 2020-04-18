957533863e7d8928b9238b6f2d2aade8f56d5676
Crash Override <emmettmathison@gmail.com>
2020-04-18 14:26:18 -0700

    okay.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
+const path = require('path')
 const PORT = process.env.PORT || 5000
-const path = require("path")
 
 express()
   .use(express.static(path.join(__dirname, 'client')))
